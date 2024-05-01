d6fbb4fbb39b1e1f880d21e301387de6348f06a7
Kallam Tarun Kumar Reddy <kallamtarun5@gmail.com>
2024-05-01 15:59:04 +0530

    Added files

--- Cpp files/neural_network.cpp
+++ Cpp files/neural_network.cpp
@@ -5,7 +5,19 @@
 
 using namespace std;
 
-class neural_network{
-public:
+struct hidden_layer{
+vector<int> layer;
+};
 
+class neural_network{
+private:
+
+public:
+    vector <int> input_layer;
+    void establish_neural_network (int number_of_hidden_layers, int length_of_input_layer)
+    {
+        hidden_layer[number_of_hidden_layers];
+        for (int i = 0; i < length_of_input_layer)
+            input_layer.push_back(0);
+    }
 };
